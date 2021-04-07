2bbd5f7e1c6d8b0c12190b207d2f14d3e1f55e98
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-04-07 13:25:30 -0800

    setup

---
+++ README.md
@@ -0,0 +1,1 @@
+2sister restaurant website with react,typescript
---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+
+import React, {Component, Fragment} from "react";
+import Apptwo from "./Apptwo";
+import "./App.scss";
+import logo from "./assets/logo.svg";
+import {name} from "./appp";
+
+interface Props {
+    foo: string;
+    
+  }
+
+class App extends Component<Props, {}> {
+   
+
+   
+    render() {
+
+        return (
+
+            <Fragment>
+            <header className = "header">
+                <p>{name}</p>
+                <p>{this.props.foo}</p>
+            <div>
+             <img src={logo} />
+            </div>
+                <div>
+                    <h1>Main Header</h1>
+                </div>
+
+                <div>
+                    <p>Description: Hello World from webpack and typescript</p>
+                </div>
+
+                <Apptwo />
+
+                </header>
+
+            </Fragment>
+
+        );
+    }
+}
+
+export default App;
