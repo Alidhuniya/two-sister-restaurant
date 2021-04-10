2f2d5a33936e5fb2c349e97613c4e4f2836ff254
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-04-10 12:12:23 -0800

    responsive navbar added with css only

--- src/components/reuse/Navbarview.tsx
+++ src/components/reuse/Navbarview.tsx
@@ -4,16 +4,28 @@ import "./../../sass/2-components/_navbarview.scss";
      const nav= props.nav;
     //  console.log(nav);
    const list = nav.slice(0).reverse().map((listItems) => 
-   <div key = {listItems.sys.id} className="nav">
+   <li className="navbar__li" key = {listItems.sys.id}>
+     <a href="#" className="a">
      <img  className="nav__img" src={listItems.fields.navimg.fields.file.url} alt="nav-logo" /> 
        <h1 className="nav__h1">{listItems.fields.navtext}</h1>
-       </div>
+       </a>
+       </li>
+
+       
+       
    )
     
     
      return (
-         <div>
-         {list}
-         </div>
+         <header className="header">
+           <input className="menu-btn" type="checkbox" id="menu-btn" />
+    <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+             {/* <nav className="navbar__block"> */}
+             <ul className="menu">
+                 {list}
+         </ul>
+        {/* </nav> */}
+         </header>
      );
  }
+
