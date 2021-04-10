a8c0c91167be9b3e4bd2fbe8d22bb69135866f5f
alidhuniya <alihussaindhuniya0307@gmail.com>
2021-04-10 14:34:01 -0800

    fb bots messanger added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { Fragment } from "react";
 import {NavbarContainer} from "./components/reuse/NavbarContainer";
 import {Googlemap} from "./components/reuse/Googlemap";
-
+import {Fbots} from "./components/reuse/Fbots";
 
  export const  App = () => {
   return (
     <Fragment>
      <div>
        {/* <NavbarContainer /> */}
-      <Googlemap />
+      {/* <Googlemap /> */}
+      <Fbots />
      </div>
     </Fragment>
   )
---
+++ src/components/reuse/Fbots.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+export const Fbots = () => {
+    return (
+        <div>
+    <MessengerCustomerChat
+      pageId="108153734298160"
+      appId="<YOUR_APP_ID>"
+    />
+  </div>
+    )
+}
